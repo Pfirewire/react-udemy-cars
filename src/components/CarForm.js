776c8e527bbf5a795225a9dddc5fed6c54d3fe19
Stephen Pflug <stephen@pflug.us>
2023-02-17 06:07:31 -0600

    Form Values to Update State

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,9 +1,31 @@
-
+import { useDispatch, useSelector } from "react-redux";
+import { changeName, changeCost } from "../store";
 
 function CarForm() {
+    const dispatch = useDispatch();
+    const name = useSelector((state) => {
+        return state.form.name;
+    });
+
+    const handleNameChange = (e) => {
+        dispatch(changeName(e.target.value));
+    };
+
     return(
-        <div>
-            CarForm
+        <div className='car-form panel'>
+            <h4 className='subtitle is-3'>Add Car</h4>
+            <form>
+                <div className='field-group'>
+                    <div className='field'>
+                        <label className='label'>Name</label>
+                        <input
+                            className='input is-expanded'
+                            value={name}
+                            onChange={handleNameChange}
+                        />
+                    </div>
+                </div>
+            </form>
         </div>
     );
 }
